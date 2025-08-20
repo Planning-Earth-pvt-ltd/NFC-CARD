7292270b0ed0f1d3d7ff70efd76a622f2e1c62f4
Ubuntu <ubuntu@ip-172-31-34-208.ec2.internal>
2025-08-20 10:31:55 +0000

    updated DB.js config file

--- config/DB.js
+++ config/DB.js
@@ -1,15 +1,23 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('nfc_cards', 'root', 'Root@123', {
-  host: 'localhost',
-  dialect: 'mysql',
-
-  dialectOptions: {
-    connectTimeout: 10000  // ✅ Valid alternative to acquireTimeout
-  },
-  define: {
-    charset: 'utf8',
-    collate: 'utf8_general_ci'
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST, // "mysql"
+    port: process.env.DB_PORT, // 3306
+    dialect: process.env.DB_DIALECT, // "mysql"
+    dialectOptions: {
+      connectTimeout: 10000
+    },
+    define: {
+      charset: 'utf8',
+      collate: 'utf8_general_ci'
+    }
   }
-});
+);
+
 module.exports = sequelize;
+
